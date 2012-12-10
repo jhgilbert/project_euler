588e35ec1060909dd424b98881896704b1dedc81
jhgilbert <j.h.gilbert@gmail.com>
2012-12-09 23:46:08 -0800

    Solve #3, but still too slowly

--- 3.rb
+++ 3.rb
@@ -6,28 +6,25 @@ require_relative "timer"
 
 def find_biggest_prime_factor(n)
 	n2 = n + 1
-	mod = nil
 	loop do
 		n2 -= 1
 		mod = n % n2
-		if mod == 0
-			status = is_prime?(n2)
-		end
-	break if status == true
+	break if mod == 0 and is_prime?(n2)
 	end
-	n2
+	n2 
 end
-	
+
 def is_prime?(n)
-	i = 0
-	n2 = n
+	n2 = Math.sqrt(n).round
+	indicator = 0
 	until n2 == 2
-		if (n % (n2 - 1)) == 0
-			i += 1
+		mod = n % n2
+		if mod == 0
+			indicator += 1
 		end
 		n2 -= 1
 	end
-	if i == 0
+	if indicator == 0
 		true
 	else
 		false
--- timer.rb
+++ timer.rb
@@ -2,6 +2,6 @@ def timer
 	start = Time.now.to_f
 	result = yield
 	finish = Time.now.to_f
-	elapsed_time = (finish - start).round # Result just needs to clock in at about a minute or less
+	elapsed_time = (finish - start)
 	"Processed in #{elapsed_time} seconds with a result of #{result}."
 end
